8a91245a4b9ae719b86b13e27d1aa0ce1484a1b2
Joao Gessel <joao.vangessel@edu-kw1c.nl>
2025-08-05 15:39:31 +0200

    API connectie gemaakt / producten tonen

---
+++ app/persLib/users.ts
@@ -0,0 +1,13 @@
+// João van Gessel – users.ts
+// Beschrijving: Simulatie van gebruikersdata voor het inloggen.
+// Bewaart een gebruiker in een aparte file voor overzicht.
+// Ik heb gekozen voor een extern bestand om het toch iets overzichtelijker te houden, ookal is het maar een klein project/simulatie van inlog
+
+//gebruiker aanmaken en opslaan
+export const users = 
+[
+  {
+    email: "test123@test.com",
+    password: "Nadya",
+  },
+];
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,25 @@
-import Navbar from "../components/navbar"; // pas het pad aan als nodig
+import Navbar from "../components/navbar"; 
+import { useEffect, useState } from "react";
+
+type Product = {
+  id: number;
+  title: string;
+  price: number;
+  thumbnail: string;
+};
+
 
 export default function Home() {
+
+  const [products, setProducts] = useState<Product[]>([]);
+
+   useEffect(() => {
+    fetch("https://dummyjson.com/products")
+      .then((res) => res.json())
+      .then((data) => setProducts(data.products));
+  }, []);
+
+
   return (
     <div className="min-h-screen bg-red-100">
       <Navbar />
@@ -8,15 +27,25 @@ export default function Home() {
       <div className="p-6">
         <div className="max-w-6xl mx-auto px-2 mt-10">
         <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-4 justify-items-center">
-          {Array.from({ length: 15 }).map((_, index) => (
-            <div
-              key={index}
-              className="bg-[#493545] p-4 rounded-xl shadow h-64 w-[200px]"
-            ></div>
-          ))}
+          {products.map((product) => (
+        <div
+          key={product.id}
+          className="bg-[#493545] p-4 rounded-xl shadow h-64 w-[200px] text-white"
+        >
+          <img
+            src={product.thumbnail}
+            alt={product.title}
+            className="w-full h-24 object-cover rounded mb-2"
+          />
+          <h2 className="text-sm font-bold truncate">{product.title}</h2>
+          <p className="text-xs">€{product.price}</p>
+          <button className="mt-2 bg-black px-3 py-1 text-sm rounded">Add to cart</button>
+        </div>
+        ))}
         </div>
       </div>
-      </div>
-    </div>
+
+  </div>
+  </div>
   );
 }
